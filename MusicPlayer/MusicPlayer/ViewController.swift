ec0cb7cd49ca0ccb8f5df7a0b973e8044e33445b
Danna <songda515@gmail.com>
2021-06-06 16:49:47 +0900

    [MusicPlayer] 프로젝트 생성

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Dain Song on 2021/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
