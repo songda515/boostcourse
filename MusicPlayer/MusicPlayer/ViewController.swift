5c7098e5e48e6578b5fc21e1d4bde5f7b4d6c93e
Danna <songda515@gmail.com>
2021-06-08 19:09:37 +0900

    [MusicPlayer] 인터페이스 빌더(Storyboard)로 UI 개발, Autolayout 적용

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.addViewWithCode()
+        //self.addViewWithCode()
         self.initializePlayer()
     }
 
